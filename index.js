a82c03a87feb8d0180522fbbac31715d438b1dfd
Alex Rosengarten <alxr@google.com>
2021-02-19 17:31:41 -0800

    Creating simpler greeter service.

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
+
 /**
  * Respond with hello worker text
  * @param {Request} request
  */
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+  switch (request.method) {
+    case 'POST':
+      const json = request.json()
+      const name = json.name || 'friend';
+      return new Response(`Hello, ${name}.`)
+    default:
+      return new Response(`Please use french press.`, {status: 418});
+  }
 }
